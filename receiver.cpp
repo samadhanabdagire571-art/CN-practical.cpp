89d6b4fac094b4cd1fa45097d8a6f458069d6a22
samadhanabdagire571-art <samadhanabdagire571@gmail.com>
2026-08-12 04:51:57 +0000

    change

--- receiver.cpp
+++ receiver.cpp
@@ -1,2 +1,39 @@
-#include<iostream>
-#include<string>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string input;
+    int count = 0;
+
+    std::cout << "Enter received bits: ";
+    std::cin >> input;
+
+    std::cout << "Destuffed: ";
+
+    for (int i = 0; i < input.length(); i++)
+    {
+        char c = input[i];
+
+        std::cout << c;
+
+        if (c == '1')
+        {
+            count++;
+
+            if (count == 5)
+            {
+                i++;          // Skip the stuffed 0
+                count = 0;
+            }
+        }
+        else
+        {
+            count = 0;
+        }
+    }
+
+    std::cout << std::endl;
+
+    return 0;
+}
